eef1318ac27b4356cc2949b8ea0ea7779598e2c2
omar mahmood sultani <bromar2292@googlemail.com>
2024-04-14 19:41:33 +0100

    secret

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+const kParseApplicationId = "JrijWLy8NuWA4oQxc4dezbbZR7lJp8xeCObjgKVx";
+const kParseRestApiKey = "Q9iwTeJgLHEG6G5QbPyT7vup3ufaMwHpf3gWcNs4";
+const kParseClientKey = "Gj2FBfFuGkrIqfPkiaSpU4VwAEbE6ec6ycfithMK";
